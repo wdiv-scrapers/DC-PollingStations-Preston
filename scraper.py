f62c774869bebef70a2de8a76ad118c325ab2af1
chris48s <chris.shaw480@gmail.com>
2018-07-26 15:53:51 +0100

    init

---
+++ scraper.py
@@ -0,0 +1,12 @@
+from dc_base_scrapers.geojson_scraper import RandomIdGeoJSONScraper
+
+
+stations_url = "http://maps.preston.gov.uk/electionsdemocracyclub/wfs.exe?service=WFS&version=2.0.0&request=GetFeature&typeNames=ns%3APolling_Stations&outputFormat=application%2Fvnd.geo%2Bjson"
+districts_url = "http://maps.preston.gov.uk/electionsdemocracyclub/wfs.exe?service=WFS&version=2.0.0&request=GetFeature&typeNames=ns%3AElection_District_Boundaries&outputFormat=application%2Fvnd.geo%2Bjson"
+council_id = 'E07000123'
+
+
+stations_scraper = RandomIdGeoJSONScraper(stations_url, council_id, 'utf-8', 'stations')
+stations_scraper.scrape()
+districts_scraper = RandomIdGeoJSONScraper(districts_url, council_id, 'utf-8', 'districts')
+districts_scraper.scrape()
