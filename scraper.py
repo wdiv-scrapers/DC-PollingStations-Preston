fd1a6d012bb6a0c5c54614dfe97d16f9011d388c
GeoWill <roper.william@gmail.com>
2021-05-05 14:03:19 +0100

    Update scraper urls

--- scraper.py
+++ scraper.py
@@ -1,8 +1,8 @@
 from dc_base_scrapers.geojson_scraper import RandomIdGeoJSONScraper
 
 
-stations_url = "http://maps.preston.gov.uk/electionsdemocracyclub/wfs.exe?service=WFS&version=2.0.0&request=GetFeature&typeNames=ns%3APolling_Stations&outputFormat=application%2Fvnd.geo%2Bjson"
-districts_url = "http://maps.preston.gov.uk/electionsdemocracyclub/wfs.exe?service=WFS&version=2.0.0&request=GetFeature&typeNames=ns%3APolling_District_Boundaries&outputFormat=application%2Fvnd.geo%2Bjson"
+stations_url = "http://maps.preston.gov.uk/electiondemocracyclub/wfs.exe?service=WFS&version=2.0.0&request=GetFeature&typeNames=ns%3APolling_Stations&outputFormat=application%2Fvnd.geo%2Bjson"
+districts_url = "http://maps.preston.gov.uk/electiondemocracyclub/wfs.exe?service=WFS&version=2.0.0&request=GetFeature&typeNames=ns%3APolling_District_Boundaries&outputFormat=application%2Fvnd.geo%2Bjson"
 council_id = 'E07000123'
 
 
